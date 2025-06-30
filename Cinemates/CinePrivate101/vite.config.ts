97effcf0453b56cdd0b7e7ef71a471fc9aabc327
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2025-06-30 12:00:11 +0530

    Update vite.config.ts

--- Cinemates/CinePrivate101/vite.config.ts
+++ Cinemates/CinePrivate101/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0', // 👈 Bind to all network interfaces
+    port: 3021,       // 👈 Optional, default is 5173
   },
 });
